9eec584bcc401220ba7b99b49c7f0d4543429a20
Prachi <prachikale136@gmail.com>
2023-06-17 00:42:42 +0530

    [Prachi] Add: Check whether two no are equal or not

---
+++ ProgramConstruct/ProgramConstruct/IfStatement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramConstruct
+{
+    internal class IfStatement
+    {
+        public static void CheckInteger()
+        {
+            Console.WriteLine("Enetr the first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 == num2)
+            {
+                Console.WriteLine("Both integer are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both number are not equal");
+            }
+        }
+    }
+}
---
+++ ProgramConstruct/ProgramConstruct/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ProgramConstruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            IfStatement.CheckInteger();
+        }
+    }
+}
+
